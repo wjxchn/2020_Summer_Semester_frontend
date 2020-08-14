9f22bf67cd9daee4cc4677f45fd38bcb088fe1b1
QingYuAnWayne <zhangxuwei@buaa.edu.cn>
2020-08-14 15:02:45 +0800

    模板文档页面更新

--- SmallSemesterVue/src/components/DocForm.vue
+++ SmallSemesterVue/src/components/DocForm.vue
@@ -5,7 +5,7 @@
             <el-aside class="leftside" width="210px">
 
             <!--     ↓↓↓↓↓↓↓↓↓↓↓↓↓↓  这个default-active是指当前激活的页面，把页面对应的index写进去，例如：当前页面是我的文档，则写进去2-1 -->
-            <el-menu default-active="2-1" class="el-menu-vertical-demo" :default-openeds="opend"
+            <el-menu default-active="" class="el-menu-vertical-demo" :default-openeds="opend"
             @open="handleOpen" @close="handleClose" background-color="#fff"
             text-color="#000000" active-text-color="#f96332" >
             <el-submenu index="1">
@@ -38,51 +38,31 @@
             </el-menu>
             </el-aside>
         <el-container>
-        <main id="mainPart" role="main" class="container">
-        <div class="header">
+        <main id="mainPart" role="main" class="container" color="black">
+
+    <!-- 页面内部的内容写在main中间即可 -->
+        <div class="header" style="padding:20px">
             <el-breadcrumb separator-class="el-icon-arrow-right">
             <el-breadcrumb-item :to="{ path: '/Personaldoc' }">我的文档</el-breadcrumb-item>
             <el-breadcrumb-item>新建文档</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
-            <div class="plaintext_new">
-            <br>
-                <el-form ref="form" :model="form" label-width="80px">
-                <el-form-item label="文档名">
-                    <el-col :span="22">
-                    <el-input v-model="form.doc_name"></el-input>
-                    </el-col>
-                </el-form-item>
-                <el-form-item label="简介">
-                    <el-col :span="22">
-                    <el-input type="textarea" :rows="4"  v-model="form.introduction"></el-input>
-                    </el-col>
-                </el-form-item>
-                <el-form-item label="文件位置">
-                    <el-col :span="20">
-                    <el-input v-model="form.address" value="/个人文档" :disabled=true></el-input>
-                    </el-col>
-                    <el-col class="line" :span="2"></el-col>
-                    <el-col :span="3">
-                    <el-button>更改</el-button>
-                    </el-col>
-                </el-form-item>
-                </el-form>
-                <quill-editor class="editor"
-                ref="myTextEditor"
-                v-model="content"
-                style="height:100%;width:92%"
-                :options="editorOption"
-                @blur="onEditorBlur($event)"
-                @focus="onEditorFocus($event)"
-                @ready="onEditorReady($event)"
-                @change="onEditorChange($event)">
-                </quill-editor>
-
-                <br><br><br>
-                 <el-button type="primary" class="btn btn-primary" style="background-color:#f96332;color:white;float:right" @click="submitPlainText" plain>提交</el-button>
-
+        <div >
+        <el-row :gutter="20">
+            <el-col :span="8" v-for="item in DocFormData" :key="item.index">
+                <el-card :body-style="{ padding: '0px' }" style="width:90%;margin:10px" >
+                <img v-bind:src="item.url" class="image">
+                <div style="padding: 14px;">
+                    <p style="text-align:center;font-size:18px">{{item.name}}</p>
+                    <div class="bottom clearfix">
+                        <el-button type="primary" class="button" style="float:right">创建</el-button>
+                    </div>
                 </div>
+                </el-card>
+            </el-col>
+        </el-row>
+        </div>
+
         </main>
         </el-container>
         </el-container>
@@ -93,24 +73,44 @@
 <script>
 import Guider from '../components/Guider'
 import BottomGuider from '../components/BottomGuider'
-import axios from 'axios'
 export default {
-    name: 'PlainText_new',
+    name: 'DocForm',
     components: {
         Guider,
         BottomGuider
     },
     data () {
-        return {
+       return {
+            currentDate: new Date(),
             opend:['1','2','3'],
             uniqueOpened:false,
             istabBar: false,
-            content: null,
-            form:null,
-            editorOption: {
-
-                modules: {
-                    //侧边栏的跳转
+            DocFormData:[
+                {
+                    index:"1",
+                    url:"http://m.qpic.cn/psc?/V50IFnJp4XxV2s3cUReP2J6k3C3FNeXZ/bqQfVz5yrrGYSXMvKr.cqal0AN6VQ0iw2YAmTiXSTlQWFKocisyr4FdC.N2I3kH32qPK41I1ovIrf4ToCYLDjrFbCS5JBcplEHLnwOX09M8!/b&bo=iwE1AgAAAAADB58!&rf=viewer_4",
+                    name:"空白文档"
+                },
+                {
+                    index:"2",
+                    url:"http://m.qpic.cn/psc?/V50IFnJp4XxV2s3cUReP2J6k3C3FNeXZ/bqQfVz5yrrGYSXMvKr.cqVYbsi3361Tl8U.RzQolBhjzyp1f6JVmaATAaUfQFJGXvvHvv5Fv8.Bp8xcRutbzK0NDXIvMBqJ3ft7FfeAMtOs!/b&bo=gQKJAwAAAAADBys!&rf=viewer_4",
+                    name:"2019年终总结通用模板"
+                },
+                {
+                    index:"3",
+                    url:"http://m.qpic.cn/psc?/V50IFnJp4XxV2s3cUReP2J6k3C3FNeXZ/TmEUgtj9EK6.7V8ajmQrEE3HyXMqX8rSCLTDyTAF8MW8NSvqDvHUtkAKqFy68jrJzqn9POwNla2oXQgooKgOmxlIbY4b15Jwxt80YXenGCo!/b&bo=fAKJAwAAAAADJ*Y!&rf=viewer_4",
+                    name:"个人借款协议（无居间人）"
+                },
+                {
+                    index:"4",
+                    url:"http://m.qpic.cn/psc?/V50IFnJp4XxV2s3cUReP2J6k3C3FNeXZ/bqQfVz5yrrGYSXMvKr.cqSzkACjEy70*RAD5.Da9Vv0*.q5OOV.DmMQ2edWjzTtbnBk1J.1iI3duH9INwOep4me5QaDpo1dvSiR.LeXU.7s!/b&bo=fwKKAwAAAAADB9Y!&rf=viewer_4",
+                    name:"外贸合同"
+                }
+            ]
+        }
+    },
+    methods: {
+        //侧边栏的跳转
         handleOpen(key, keyPath) {
             console.log(key, keyPath);
         },
@@ -147,72 +147,6 @@ export default {
         Aboutus(){
             this.$router.push('/Aboutus');
         },
-                toolbar: [
-                    ["bold", "italic", "underline", "strike"], // 加粗 斜体 下划线 删除线
-                    ["blockquote", "code-block"], // 引用  代码块
-                    [{ header: 1 }, { header: 2 }], // 1、2 级标题
-                    [{ list: "ordered" }, { list: "bullet" }], // 有序、无序列表
-                    [{ script: "sub" }, { script: "super" }], // 上标/下标
-                    [{ indent: "-1" }, { indent: "+1" }], // 缩进
-                    // [{'direction': 'rtl'}],                         // 文本方向
-                    [{ size: ["small", false, "large", "huge"] }], // 字体大小
-                    [{ header: [1, 2, 3, 4, 5, 6, false] }], // 标题
-                    [{ color: [] }, { background: [] }], // 字体颜色、字体背景颜色
-                    [{ font: [] }], // 字体种类
-                    [{ align: [] }], // 对齐方式
-                    ["clean"], // 清除文本格式
-                    ["link", "image"] // 链接、图片、视频
-                ], //工具菜单栏配置
-                },
-                placeholder: '请在此输入正文', //提示
-                readyOnly: false, //是否只读
-                theme: 'snow', //主题 snow/bubble
-                syntax: true, //语法检测
-        },
-             form: {
-                doc_name: '',
-                introduction:'',
-                address:''
-                 }
-        }
-    },
-    methods: {
-         // 失去焦点
-        onEditorBlur(editor) {},
-        // 获得焦点
-        onEditorFocus(editor) {},
-        // 开始
-        onEditorReady(editor) {},
-        // 值发生变化
-        onEditorChange(editor) {
-            this.content = editor.html;
-            console.log(editor);
-        },
-        submitPlainText(){
-            console.log(this.content);
-            axios({
-                method: 'post',
-                url: 'http://localhost:8000/api/addpersonaldoc/',
-                data: {'content': this.content, 'doc_name': this.form.doc_name, 'introduction': this.form.introduction, 'doc_creater': localStorage.getItem('username')}
-            })
-            .then(response => {
-                console.log(response)
-                if(response.data.code===200){
-                    alert('添加富文本成功')
-                    this.$router.push('/')
-                }
-                else if(response.data.code===400){
-                    alert('添加富文本失败')
-                }
-                else{
-                    alert('错误')
-                }
-            })
-            .catch(error => {
-                console.log(error)
-                alert('出现错误')
-            })
-        },
         // 添加一个方法 兼容
         handleScroll () {
             let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
@@ -229,11 +163,6 @@ export default {
                 }
         }
     },
-    computed: {
-        editor() {
-            return this.$refs.myTextEditor.quillEditor;
-        }
-    },
     mounted () {
         window.addEventListener('scroll', this.handleScroll); // Dom树加载完毕
     },
@@ -244,9 +173,6 @@ export default {
 </script>
 
 <style scoped>
-.header{
-    margin-top:2%;
-}
 .isFixed {
     position: fixed;
     top: 0;
@@ -260,4 +186,29 @@ export default {
 #mainPart {
     width: 100%;
 }
+ .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .image {
+    width: 100%;
+    height: 450px;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
 </style>
--- SmallSemesterVue/src/components/PersonalDoc.vue
+++ SmallSemesterVue/src/components/PersonalDoc.vue
@@ -38,17 +38,14 @@
             </el-menu>
             </el-aside>
         <el-container>
-        <main id="mainPart" role="main" class="container">
+        <main id="mainPart" role="main" class="container">  
             <div class="personaldoc" >
+            <div style="padding-top:20px">
                 <div style="float:right">
                 <el-input  placeholder="请输入内容" style="width:250px">
                 <el-button  slot="append" icon="el-icon-search"></el-button></el-input>
-                <router-link to='/PlainText_new'>
+                <router-link to='/DocForm'>
                     <el-button style="background-color:#f96332;color:white"  >新建 </el-button>
-                    
-                </router-link>
-                <router-link to='/CreateFromDemo'>
-                   <el-button style="background-color:#f96332;color:white"  >使用模板创建 </el-button>
                     
                 </router-link>
                 </div>
@@ -57,6 +54,7 @@
                 <span v-html="htmlData">
                     {{htmlData}}
                 </span>
+            </div>
                 <br>
                 <p class="history"> 最近浏览记录 </p>
                 
