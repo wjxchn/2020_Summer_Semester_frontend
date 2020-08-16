6e2f770cf053b061b7c4cd3c67839903af599fea
Gavin_lrn <54489982+gavin-lren@users.noreply.github.com>
2020-08-16 20:16:34 +0800

    个人资料页面样式设计更新

--- SmallSemesterVue/src/components/Person.vue
+++ SmallSemesterVue/src/components/Person.vue
@@ -40,22 +40,76 @@
         <main id="mainPart" role="main" class="container">
             <div class="div1">
                 <div class="div2">
-                    <div class="picture" style="width:100%;height:70%;background-color:transparent;">
-                        <el-button class="but" size="small" plain ><i class="el-icon-camera" style="color:white" ></i>上传封面图片</el-button>
+                        <div class="block" v-for="fit in fits" :key="fit">
+                            <el-image
+                            style="width: 100%;height:200px"
+                            :src="backgroundurl"
+                            :fit="fit"></el-image>
+                        </div>
+                       
+                        <el-dialog title="上传封面照片" :visible.sync="sendCoverVisible">
+                            <ImgCutter
+                                ref="imgCutterModal"
+                                label="Crop local pictures"
+                                :crossOrigin="true"
+                                crossOriginHeader="*"
+                                rate=""
+                                toolBgc="none"
+                                :isModal="false"
+                                :showChooseBtn="true"
+                                :lockScroll="true"
+                                :boxWidth="545"
+                                :boxHeight="458"
+                                :cutWidth="250"
+                                :cutHeight="250"
+                                :sizeChange="true"
+                                :moveAble="true"
+                                :originalGraph="false"
+                                @cutDown="cutDownCover">
+                                <div class="btn btn-primary" slot="open">剪裁本地背景图片</div>
+                            </ImgCutter>
+                        </el-dialog>
+                        <el-dialog title="上传头像照片" :visible.sync="sendProfileVisible">
+                            <ImgCutter
+                                ref="imgCutterModal"
+                                label="Crop local pictures"
+                                :crossOrigin="true"
+                                crossOriginHeader="*"
+                                rate=""
+                                toolBgc="none"
+                                :isModal="false"
+                                :showChooseBtn="true"
+                                :lockScroll="true"
+                                :boxWidth="545"
+                                :boxHeight="458"
+                                :cutWidth="250"
+                                :cutHeight="250"
+                                :sizeChange="true"
+                                :moveAble="true"
+                                :originalGraph="false"
+                                @cutDown="cutDownProfile">
+                                <div class="btn btn-primary" slot="open">剪裁本地背景图片</div>
+                            </ImgCutter>
+                        </el-dialog>
                     <div class="block1">
                     <el-row >
                             <el-col  span="4" >
                                 <el-avatar shape="square" :size="150" :src="circleUrl"></el-avatar>
                             </el-col> 
-                            <el-col class="eman" span="6" > 
+                            <el-col class="eman" span="10" > 
                                 <h2 :value="showusername">{{showusername}}</h2>
                             </el-col>
                             <el-col class="edit" span="3" > 
-                                
                                 <el-button style="background-color:#f96332;color:white"  round @click="EditPerson" :loading="logining">编辑个人信息</el-button>
                             </el-col>
+                            <el-col class="edit" span="3.5" > 
+                                <el-button style="background-color:#f96332;color:white"  round @click="sendCoverVisible = true" plain ><i class="el-icon-camera" style="color:white" ></i>上传封面图片</el-button>
+                            </el-col>
+                            <el-col class="edit" span="3.5" >    
+                                <el-button style="background-color:#f96332;color:white"  round @click="sendProfileVisible = true" plain ><i class="el-icon-camera" style="color:white" ></i>上传头像图片</el-button>
+                            </el-col>
+                            
                         </el-row>
-                    </div>
                     </div>
                 </div>
                 <div class="div3" >
@@ -102,11 +156,13 @@
 import Guider from '../components/Guider'
 import BottomGuider from '../components/BottomGuider'
 import axios from 'axios'
+import ImgCutter from 'vue-img-cutter'
 export default {
     name: 'PageDemo',
     components: {
         Guider,
-        BottomGuider
+        BottomGuider,
+        ImgCutter
     },
     data () {
         return {
@@ -118,7 +174,11 @@ export default {
             tableData2: [],
             circleUrl: "http://r.photo.store.qq.com/psc?/V143D3j445iBwL/ubiEST8aMMlZjEEUGVmWIvQRktHhz2iAdR3J.A4nqij8aa0.iu6BoAsF9QicJOl2HSbaNWgM8nnAiFqjhgVj6jpHhRfm2MRX08O2SAq4NIQ!/r",
             showusername:"这里是用户昵称",
-            istabBar: false
+            istabBar: false,
+            sendCoverVisible: false,
+            sendProfileVisible: false,
+            fits: ['fill'],
+            backgroundurl: "http://r.photo.store.qq.com/psc?/V52Qutst1Ze0TP0BqSlg0Ogc8N1lTYZp/45NBuzDIW489QBoVep5mcWCQKX9WHhAARbvjDHbE5p080qFMB3PE9EKYZm4ixFpGpOz2LgI6LvNURNSZH29x.*XMFGLDpA3riSY4BnJRS8k!/r"
         }
         
     },
@@ -158,7 +218,7 @@ export default {
             this.$router.push('/Aboutus');
         },
         //
-         EditPerson(){
+        EditPerson(){
             this.$router.push('/EditPerson');
         },
         // 添加一个方法 兼容
@@ -175,7 +235,55 @@ export default {
                     this.istabBar = false
                     mainPart.style.paddingTop = "0px";
                 }
-        }
+        },
+        cutDownCover(res){
+            let imgSrc = res.dataURL;
+            console.log(imgSrc);//输出为base64数据
+            axios({
+                method: 'post',
+                url: 'http://localhost:8000/api/addbackgroundphoto/',
+                data: { 'username': localStorage.getItem('username'), 'backgroundphotodata': imgSrc}
+            })
+            .then(response => {
+                if(response.data.code === 200){
+                    alert('添加背景图片成功，请手动刷新')
+                }
+                else if(response.data.code === 400){
+                    alert('添加背景图片失败')
+                }
+                else{
+                    alert('错误')
+                }
+            })
+            .catch(error => {
+                alert('出现错误')
+            })
+            this.sendCoverVisible = false                       
+        },
+        cutDownProfile(res){
+            let imgSrc = res.dataURL;
+            console.log(imgSrc);//输出为base64数据
+            axios({
+                method: 'post',
+                url: 'http://localhost:8000/api/addprofilephoto/',
+                data: { 'username': localStorage.getItem('username'), 'profilephotodata': imgSrc}
+            })
+            .then(response => {
+                if(response.data.code === 200){
+                    alert('添加头像图片成功，请手动刷新')
+                }
+                else if(response.data.code === 400){
+                    alert('添加头像图片失败')
+                }
+                else{
+                    alert('错误')
+                }
+            })
+            .catch(error => {
+                alert('出现错误')
+            })
+            this.sendProfileVisible = false                       
+        }        
     },
     created: function(){
         axios({
@@ -205,7 +313,55 @@ export default {
             console.log(error)
             alert('错误')
             this.$router.go(0)
-        })             
+        });
+        axios({
+            method: 'post',
+            url: 'http://localhost:8000/api/showbackgroundphoto/',
+            data: {'username': localStorage.getItem('username')}
+        })
+        .then(response => {
+            console.log(response)
+            if(response.data.code === 400){
+                alert('加载背景图片失败')
+                this.$router.go(0)                
+            }
+            else if(response.data.code === 200){
+                this.$set(this, 'backgroundurl', response.data.backgroundphotodata)
+            }
+            else{
+                alert('错误')
+                this.$router.go(0)
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            alert('错误')
+            this.$router.go(0)
+        });
+        axios({
+            method: 'post',
+            url: 'http://localhost:8000/api/showprofilephoto/',
+            data: {'username': localStorage.getItem('username')}
+        })
+        .then(response => {
+            console.log(response)
+            if(response.data.code === 400){
+                alert('加载背景图片失败')
+                this.$router.go(0)                
+            }
+            else if(response.data.code === 200){
+                this.$set(this, 'circleUrl', response.data.profilephotodata)
+            }
+            else{
+                alert('错误')
+                this.$router.go(0)
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            alert('错误')
+            this.$router.go(0)
+        });          
     },
     mounted () {
         window.addEventListener('scroll', this.handleScroll); // Dom树加载完毕
@@ -213,27 +369,23 @@ export default {
     destroyed () {
         window.removeEventListener('scroll', this.handleScroll) // 销毁页面时清除
     }
+        
 }
 </script>
 
 <style>
 .block1{
-    padding-top: 100px;
+    margin-top:-100px;
     padding-left:20px;
 }
 .eman{
     padding-top:105px;
 }
 .edit{
-    padding-top:105px;
+    padding-top:100px;
     float:right;
 }
-.but{
-    float:right;
-    margin-top:10px;
-    margin-right:10px;
-    background-color:unset;
-}
+
 *{
     margin:0;
 }
