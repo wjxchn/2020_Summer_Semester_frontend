2fc88a3487e4c07bc715353502a9e11f68ddcd6f
Gavin_lrn <54489982+gavin-lren@users.noreply.github.com>
2020-08-18 20:17:44 +0800

    编辑个人文档页面更新

--- SmallSemesterVue/src/components/EditPersonalDoc.vue
+++ SmallSemesterVue/src/components/EditPersonalDoc.vue
@@ -56,7 +56,7 @@
                 <quill-editor class="editor"
                 ref="myTextEditor"
                 v-model="content"
-                style="height:800px"
+                style="height:1000px"
                 :options="editorOption"
                 @blur="onEditorBlur($event)"
                 @focus="onEditorFocus($event)"
